951e67ec13024685b3a0e56c4f9f9a8d5c4535fb
Hee Su Chung <jhschung88@gmail.com>
2024-02-24 11:55:08 -0500

    feat: user story 1, displays output in brief and verbose mode

--- mock/src/components/REPLInput.tsx
+++ mock/src/components/REPLInput.tsx
@@ -1,6 +1,13 @@
-import { Dispatch, SetStateAction, useState } from "react";
-import "../styles/main.css";
-import { ControlledInput } from "./ControlledInput";
+import { Dispatch, SetStateAction, useState } from 'react';
+import '../styles/main.css';
+import { ControlledInput } from './ControlledInput';
+
+interface REPLInputProps{
+  // TODO: Fill this with desired props... Maybe something to keep track of the submitted commands
+  // CHANGED
+  history: string[][][],
+  setHistory: Dispatch<SetStateAction<string[][][]>>,
+}
 
 /**
  * A command-processor function for our REPL. The function returns a string, which is the value to print to history when
@@ -10,94 +17,80 @@ import { ControlledInput } from "./ControlledInput";
  * *NOT* contain the command-name prefix.
  */
 export interface REPLFunction {
-  (args: Array<string>): String[][];
+  (args: Array<string>): string[][];
 }
 
-interface REPLInputProps {
-  // TODO: Fill this with desired props... Maybe something to keep track of the submitted commands
-  // CHANGED
-  history: String[][][];
-  setHistory: Dispatch<SetStateAction<String[][][]>>;
-}
+export function REPLInput(props : REPLInputProps) {
+    const [commandString, setCommandString] = useState<string>('');
+    const [count, setCount] = useState<number>(0)
+    const [briefMode, setBriefMode] = useState<boolean>(true);
 
-const [briefMode, setBriefMode] = useState<boolean>(true);
+    /**
+     * REPLFunction for setting mode.
+     */
+    let setMode: REPLFunction;
+    setMode = function (args: Array<string>): string[][] {
+      if (args.length === 1) {
+        if (args[0] === "brief") {
+          setBriefMode(true);
+          return [["Mode set to brief"]];
+        } else if (args[0] === "verbose") {
+          setBriefMode(false);
+          return [["Mode set to verbose"]];
+        } else {
+          return [["Wrong argument provided to mode. mode <brief OR verbose>"]];
+        }
+      } else {
+        return [["Wrong number of arguments provided"]];
+      }
+    };
 
-let setMode: REPLFunction;
-setMode = function (args: Array<string>): String[][] {
-  if (args.length === 1) {
-    if (args[0] === "brief") {
-      setBriefMode(true);
-      return [["Mode set to brief"]];
-    } else if (args[0] === "verbose") {
-      setBriefMode(false);
-      return [["Mode set to verbose"]];
-    } else {
-      return [["Wrong argument provided to mode. mode <brief OR verbose>"]];
+    // map lookup to function for cmd
+    var commandFunctions: { [cmd: string]: REPLFunction } = {
+      mode: setMode,
+    };
+
+    /**
+     * Handler for Submit button. Error checks and executes valid user command.
+     */
+    function handleSubmit(commandString: string) {
+      setCount(count + 1);
+
+      const args = commandString.split(" ");
+      if (args.length === 0) {
+        // nothing entered
+        return;
+      }
+      const command: string = args[0];
+      args.shift();
+
+      if (!(command in commandFunctions)) {
+        props.setHistory([...props.history, [["Entered unrecognized command"]]]);
+        return;
+      }
+      const appropriateHandler: REPLFunction = commandFunctions[command];
+
+      props.setHistory([...props.history, appropriateHandler(args)]);
+      setCommandString("");
     }
-  } else {
-    return [["Wrong number of arguments provided"]];
-  }
-};
 
-// map lookup to function for cmd
-var commandFunctions: { [cmd: string]: REPLFunction } = {
-  mode: setMode,
-};
-
-// You can use a custom interface or explicit fields or both! An alternative to the current function header might be:
-// REPLInput(history: string[], setHistory: Dispatch<SetStateAction<string[]>>)
-export function REPLInput(props: REPLInputProps) {
-  // Remember: let React manage state in your webapp.
-  // Manages the contents of the input box
-  const [commandString, setCommandString] = useState<string>("");
-  // TODO WITH TA : add a count state
-  const [count, setCount] = useState<number>(0);
-
-  // This function is triggered when the button is clicked.
-  function handleSubmit(commandString: string) {
-    setCount(count + 1);
-
-    const args = commandString.split(" ");
-    if (args.length === 0) {
-      // nothing entered
-      return;
-    }
-    const command: string = args[0];
-    args.shift();
-
-    if (!(command in commandFunctions)) {
-      props.setHistory([...props.history, [["Entered unrecognized command"]]]);
-      return;
-    }
-    const appropriateHandler: REPLFunction = commandFunctions[command];
-
-    // run that function, passing output to props.setHistory
-    props.setHistory([...props.history, appropriateHandler(args)]);
-    setCommandString("");
-  }
-
-  /**
-   * We suggest breaking down this component into smaller components, think about the individual pieces
-   * of the REPL and how they connect to each other...
-   */
-  return (
-    <div className="repl-input">
-      {/* This is a comment within the JSX. Notice that it's a TypeScript comment wrapped in
+    /**
+     * We suggest breaking down this component into smaller components, think about the individual pieces 
+     * of the REPL and how they connect to each other...
+     */
+    return (
+        <div className="repl-input">
+            {/* This is a comment within the JSX. Notice that it's a TypeScript comment wrapped in
             braces, so that React knows it should be interpreted as TypeScript */}
-      {/* I opted to use this HTML tag; you don't need to. It structures multiple input fields
+            {/* I opted to use this HTML tag; you don't need to. It structures multiple input fields
             into a single unit, which makes it easier for screenreaders to navigate. */}
-      <fieldset>
-        <legend>Enter a command:</legend>
-        <ControlledInput
-          value={commandString}
-          setValue={setCommandString}
-          ariaLabel={"Command input"}
-        />
-      </fieldset>
-      {/* TODO: Currently this button just counts up, can we make it push the contents of the input box to the history?*/}
-      <button onClick={() => handleSubmit(commandString)}>
-        Submitted {count} times
-      </button>
-    </div>
-  );
-}
+            <fieldset>
+              <legend>Enter a command:</legend>
+              <ControlledInput value={commandString} setValue={setCommandString} ariaLabel={"Command input"}/>
+            </fieldset>
+            {/* TODO: Currently this button just counts up, can we make it push the contents of the input box to the history?*/}
+            <button onClick={() => handleSubmit(commandString)}>Submitted {count} times</button>
+        </div>
+    );
+  }
+
